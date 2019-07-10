bfa5f337fcba3fbadf1cc6f2f558c6ccda4ae610
Josemar Sabino <josemarsabino@hotmail.com>
2019-07-10 18:46:52 -0300

    Importacao WebJars - Jquery e BootStrap vai Maven Repository

--- GerenciadorTarefas/src/main/java/br/com/sada/twgerenciadortarefas/resources/HomeController.java
+++ GerenciadorTarefas/src/main/java/br/com/sada/twgerenciadortarefas/resources/HomeController.java
@@ -5,16 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-//@RequestMapping("/")
+// @RequestMapping("/")
 public class HomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	//@GetMapping("/")
-	public String home() {
-		return "home/home";
+	// @GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/home");
+		mv.addObject("mensagem", "Mensagem do Controller");
+		return mv;
 	}
 
 }
