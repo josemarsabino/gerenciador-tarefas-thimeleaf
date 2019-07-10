d6d7a5b03c447c70bf08a4428ff87671fe441206
Josemar Sabino <josemarsabino@hotmail.com>
2019-07-10 18:18:34 -0300

    Corrigindo Classe HomeControoler Anotação Estava Errada

--- GerenciadorTarefas/src/main/java/br/com/sada/twgerenciadortarefas/resources/HomeController.java
+++ GerenciadorTarefas/src/main/java/br/com/sada/twgerenciadortarefas/resources/HomeController.java
@@ -1,14 +1,18 @@
 package br.com.sada.twgerenciadortarefas.resources;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+
+@Controller
+//@RequestMapping("/")
 public class HomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
+	//@GetMapping("/")
 	public String home() {
 		return "home/home";
 	}
